83a64065b010de50bf74b80b247c7a38ba05c163
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2020-10-18 15:17:34 -0400

    Generated the first prompt asking the length of password

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+
 // Array of special characters to be included in password
-var specialCharacters = [
+const specialChar= [
     '@',
     '%',
     '+',
@@ -26,10 +37,10 @@ var specialCharacters = [
   ];
 
   // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+const lowerCasedChar = [
   'a',
   'b',
   'c',
@@ -59,7 +70,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+const upperCasedCharS = [
   'A',
   'B',
   'C',
@@ -88,22 +99,14 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// Start of Generator code
+function generatePassword () {
+  let passLength = prompt('Enter the length of your password (Min: 8 and Max: 128)')
+    if (passLength < 8 || passLength > 128) {
+      alert('ERROR! Your password length does not meet the minimum of 8 characters and maximum of 128 characters. Please try again.')
+      return;
+    }
 }
-
-// Start of password generator code 
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
